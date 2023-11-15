9c88696ce4cf1fe1808d39bb4671c9f7f87df2c1
Steve Pfeiffer <Pfeiffer.Steven@epa.gov>
2023-11-15 13:31:26 -0500

    refactoring 1

--- app_2.r
+++ app_2.r
@@ -19,6 +19,7 @@ server <- function(input, output)
 	##################################################################################
 	# Load constants
 	##################################################################################
+	output$translationGuide <- renderText("Loading constants...")
 	require(assertthat)
 	require(stringi)
 	if (!exists("dict"))
@@ -35,23 +36,26 @@ server <- function(input, output)
 	##################################################################################
 	r <- reactiveValues()
 	observe	({
+			output$translationGuide <- renderText(paste0("input$userEnteredString = ", input$userEnteredString)) 
 			if (nchar(trimws(input$userEnteredString)) == 0)
 				{
 				output$translationGuide <- renderText('Waiting.....')
 				} else	{
+						# the next line takes a couple of seconds to execute 
 						if (length(which(sapply(dict[,2], function(X){startsWith(input$userEnteredString, X)}))) != 0) 
 							{
-							r$allPossibleLatinRoots <- dict[which(sapply(dict, function(X){startsWith(input$userEnteredString, X)}))]
-							r$thisLatinRoot <- r$allPossibleLatinRoots[which(r$allPossibleLatinRoots == r$allPossibleLatinRoots[max(sapply(r$allPossibleLatinRoots, nchar))])] 
-							output$translationGuide <- renderText(paste0("r$thisLatinRoot = ", r$thisLatinRoot))
-							#if (length(r$thisLatinRoot) == 1)
-							#	{
-							#	output$translationGuide <- renderText(paste0(" r$thisLatinRoot = ", r$thisLatinRoot))
-							#	} else	{
-							#			output$translationGuide <- renderText(paste0("Something strange this way went... r$thisLatinRoot = ", r$thisLatinRoot))
-							#			}
+							r$allPossibleLatinRoots <- unique(dict[which(sapply(dict[,2], function(X){startsWith(input$userEnteredString, X)})), 2]) 
+							if (length(which(sapply(r$allPossibleLatinRoots,nchar) == nchar(input$userEnteredString))) > 0)
+								{
+								r$APLRWANTWI <- # All Possible Latin Roots Which Are Not The Word Itself 
+								  r$allPossibleLatinRoots[-which(sapply(r$allPossibleLatinRoots,nchar) == nchar(input$userEnteredString))]
+								} else	{
+										r$APLRWANTWI <- r$allPossibleLatinRoots
+										}
+							r$thisLatinRoot <- r$APLRWANTWI[which(sapply(r$APLRWANTWI,nchar) == max(sapply(r$APLRWANTWI,nchar)))]
+							output$translationGuide <- renderText(paste0(" r$thisLatinRoot = ", r$thisLatinRoot))
 							} else	{
-									output$translationGuide <- renderText("This word does not appear to be in this app's dictionary - make sure it is spelled correctly, is a regular verb, and if in a form derived from the PPP, does include a space or linking verb.") 
+									output$translationGuide <- renderText("This word does not appear to be in this app's dictionary - make sure it is spelled correctly, is a regular verb, and if in a form derived from the PPP, does not include a space or linking verb.") 
 									}
 						}
 			})
