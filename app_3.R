e7f76a90f1a961aa9bdb64cb23403c8aefe43c10
Steve Pfeiffer <Pfeiffer.Steven@epa.gov>
2023-12-21 16:38:16 -0500

    functional enough; let's leave it here

--- app_3.R
+++ app_3.R
@@ -4,7 +4,6 @@
 #allPossibleLatinRoots <- testdict[which(sapply(testdict, function(X){startsWith("Mickey", X)}))]
 #thisLatinRoot <- allPossibleLatinRoots[max(sapply(allPossibleLatinRoots, nchar))]
 #
-
 ui <- fluidPage	(
 				textInput	(
 							"userEnteredString",
@@ -13,7 +12,6 @@ ui <- fluidPage	(
 							),
 				uiOutput("translationGuide")
 				)
-
 server <- function(input, output)
 	{
 	##################################################################################
@@ -28,81 +26,87 @@ server <- function(input, output)
 		}
 	if (!exists("casePoss"))
 		{
-		casePoss <- as.matrix(read.csv("case_poss/all_conj_not_dep.csv", header=FALSE)[,1:3]) # inflection, formed from present stem or perfect stem, ConjTenseMoodVoicePersonNumber 
+		casePoss <- as.matrix(read.csv("case_poss/all_conj_not_dep.csv", header=FALSE))
 		}
 	allEndings <- unique(casePoss[,1]) # all possible endings
-	##################################################################################
-	# Begin reactive stuff
-	##################################################################################
-	r <- reactiveValues()
-	observe	({
-			if (nchar(trimws(input$userEnteredString)) == 0)
-				{
-				output$translationGuide <- renderText('Waiting.....')
-				} else	{
-						#################################################################################################
-						#  Get the Latin root and ending #
-						#################################################################################################
-						# the next line takes a couple of seconds to execute 
-						if (length(which(sapply(dict[,2], function(X){startsWith(input$userEnteredString, X)}))) != 0) 
-							{
-							r$allPossibleLatinRootRows <- unique(dict[which(sapply(dict[,2], function(X){startsWith(input$userEnteredString, X)})), ]) 
-							if (length(which(sapply(r$allPossibleLatinRootRows[,2],nchar) == nchar(input$userEnteredString))) > 0)
-								{
-								r$APLRWANTWI <- # All Possible Latin Roots Which Are Not The Word Itself 
-								  r$allPossibleLatinRootRows[-which(sapply(r$allPossibleLatinRootRows[,2],nchar) == nchar(input$userEnteredString)), 2]
-								} else	{
-										r$APLRWANTWI <- r$allPossibleLatinRootRows[,2]
-										}
-							r$thisLatinRoot <- r$APLRWANTWI[which(sapply(r$APLRWANTWI,nchar) == max(sapply(r$APLRWANTWI,nchar)))]
-							r$thisLatinEnding <- substr(input$userEnteredString, nchar(r$thisLatinRoot)+1, nchar(input$userEnteredString))
-							r$thisLatinRootInfo		<- r$allPossibleLatinRootRows[which(r$allPossibleLatinRootRows[,2] == r$thisLatinRoot), ] 
-							# [1] "3rd"          "orr"          "yada"         "perfect_stem"
-							# [2] "3rd_deponent" "orr"          "blah"         "present_stem"
-							if ("matrix" %in% class(r$thisLatinRootInfo))
-								{
-								for (i in 1:nrow(thisLatinRootInfo))
-									{
-									
-									}
-								}
-							
-							
-							r$dictHits <- dict[which(dict[,2] == r$thisLatinRoot), ]
-							if ("matrix" %in% class(r$dictHits)) # 2 or more rows returned
-								{
-								r$conjAndPP <- character(0)
-								for (i in 1:nrow(r$dictHits))
-									{
-									r$conjAndPP <- c	(
-														r$conjAndPP,
-														stringi::stri_flatten(c(r$dictHits[i,1], r$dictHits[i,4]), collapse=" ") 
-														)
-									}
-								} else	{ # 1 row returned
-										r$conjAndPP <- stringi::stri_flatten(c(r$dictHits[1], r$dictHits[4]), collapse=" ")
-										}
-							r$outMat <- matrix(nrow=2, ncol=1, data=c(paste0(r$thisLatinRoot, "-"), paste0("-", r$thisLatinEnding))) 
-							for (i in 1:length(r$conjAndPP))
-								{
-								r$outMat <- rbind(r$outMat, r$conjAndPP[i])
-								}
-							print(r$outMat)
-							print("#")
-							print("#")
-							print("#")
-							output$translationGuide <- renderTable	(
-																	r$outMat,
-																	#rownames=TRUE,
-																	colnames=FALSE
-																	)
-							} else	{
-									output$translationGuide <- renderText("This word does not appear to be in this app's dictionary - make sure it is spelled correctly, is a regular verb, and if in a form derived from the PPP, does not include a space or linking verb.") 
-									}
-						}
-			})
+	output$translationGuide <- renderTable	(
+											{
+											if (nchar(trimws(input$userEnteredString)) == 0)
+												{
+												###
+												###
+												###
+												paste0("Waiting.....")
+												###
+												###
+												###
+												} else	{
+														if (length(which(sapply(dict[,2], function(X){startsWith(input$userEnteredString, X)}))) != 0) 
+															{
+															allPossibleLatinRoots <- unique(dict[which(sapply(dict[,2], function(X){startsWith(input$userEnteredString, X)})), 2]) 
+															if (length(which(sapply(allPossibleLatinRoots,nchar) == nchar(input$userEnteredString))) > 0)
+																{
+																APLRWANTWI <- # All Possible Latin Roots Which Are Not The Word Itself 
+																  allPossibleLatinRoots[-which(sapply(allPossibleLatinRoots,nchar) == nchar(input$userEnteredString))]
+																} else	{
+																		APLRWANTWI <- allPossibleLatinRoots
+																		}
+															thisLatinRoot <- APLRWANTWI[which(sapply(APLRWANTWI,nchar) == max(sapply(APLRWANTWI,nchar)))]
+															thisLatinEnding <- substr(input$userEnteredString, nchar(thisLatinRoot)+1, nchar(input$userEnteredString))
+															dictHits <- dict[which(dict[,2] == thisLatinRoot), ]
+															if ("matrix" %in% class(dictHits)) # 2 or more rows returned
+																{
+																conjAndPP <- character(0)
+																for (i in 1:nrow(dictHits))
+																	{
+																	conjAndPP <- c	(
+																					conjAndPP,
+																					stringi::stri_flatten(c(dictHits[i,1], dictHits[i,4]), collapse=" ") 
+																					)
+																	}
+																} else	{ # 1 row returned
+																		conjAndPP <- stringi::stri_flatten(c(dictHits[1], dictHits[4]), collapse=" ")
+																		}
+															outMat <- matrix(nrow=1, ncol=3, data=c(paste0(thisLatinRoot, "-"), paste0("-", thisLatinEnding))) 
+															for (i in 1:length(conjAndPP))
+																{
+																if (endsWith(conjAndPP[i], " PPP"))
+																	{
+																	casePossibilities <- casePoss[which(apply(casePoss,1,function(X){X[1]==thisLatinEnding & X[2]=="formed_from_PPP"})), 3] 
+																	outMat[1,3] <- casePossibilities[1]
+																	if (length(casePossibilities) < 1)
+																		{
+																		for (j in 2:length(casePossibilities))
+																			{
+																			outMat <- rbind	(
+																							outMat,
+																							c("", "", casePossibilities[j])
+																							)
+																			}
+																		}
+																	} else	{
+																			outMat[1,3] <- conjAndPP[i]
+																			}
+																}
+															###
+															###
+															###
+															unique(outMat)
+															###
+															###
+															###
+															} else	{
+																	###
+																	###
+																	###
+																	paste0("This word does not appear to be in this app's dictionary - make sure it is spelled correctly, is a regular verb, and if in a form derived from the PPP, does not include a space or linking verb.") 
+																	###
+																	###
+																	###
+																	}
+														}
+											},
+											colnames=FALSE
+											)
 	}
-	
 shinyApp(ui=ui, server=server)
-
-
